4405e013e26e4d0a118ad9613788d06f0b7e1ed6
sajee.s <sajee.s@eight25media.com>
2024-10-29 02:29:21 +0530

    Organize the components structure V1.0

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,10 +3,22 @@ import Link from 'next/link';
 import ContentfulImage from '@/components/Contentfulimage';
 
 const Card = ({recipe}) => {
-    const { title, timeToCook, thumbnail, slug } = recipe.fields;
+    const { title, timeToCook, thumbnails, slug } = recipe.fields;
     console.log('title', title);
+    console.log('thumbnail', thumbnails);
   return (
-    <div className='bg-slate-500 p-5'>{title}</div>
+    <div className='w-1/3 bg-slate-50 border py-8 px-10 rounded-xl flex flex-col'>
+      <ContentfulImage 
+          src={thumbnails.fields.file.url}
+          alt={thumbnails.fields.description}
+          width={600} 
+          height={400} 
+          quality={96}
+          priority
+      />
+      <h3 className='text-lg font-bold mt-4 mb-3'>Name: {title}</h3>
+      <p className='text-md font-medium mt-auto'>Cooking Time: {timeToCook}</p>
+      </div>
   );
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,24 +2,20 @@ import { client } from "@/lib/contentful";
 import Image from "next/image";
 import localFont from "next/font/local";
 
-import Card from "@/components/card";
+
+import Recipes from "@/components/Recipes";
 
 
 export default function Home({ recipes }) {
   console.log('recipes', recipes);
   return (
-    <div
-      className={` grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
+    <>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        {recipes.map((recipe, i) => (
-          <Card key={i} recipe={recipe}/>
-        ))}
+        
+        <Recipes recipes={recipes}/>
+        
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-       <p>test footer</p>
-      </footer>
-    </div>
+    </>
   );
 }
 
